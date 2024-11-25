e89ee7e0daf6712e38859e57eaf7e1cc3b02e77e
Dax <daxnavarrete@gmail.com>
2024-11-25 09:20:41 -0500

    allow any origin request

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,6 @@ from flask_cors import CORS
 def create_app():
     app = Flask(__name__)
 
-    CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
     CORS(app)
 
     from .routes import main as main_blueprint
