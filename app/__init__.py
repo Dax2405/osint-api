5dd3c48c010ea72d958602182a6649e34e494730
Dax <daxnavarrete@gmail.com>
2024-11-25 09:16:34 -0500

    add cors to api

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,14 @@
 from flask import Flask
+from flask_cors import CORS
+
 
 def create_app():
     app = Flask(__name__)
-    
+
+    CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
+    CORS(app)
+
     from .routes import main as main_blueprint
     app.register_blueprint(main_blueprint)
-    
+
     return app
